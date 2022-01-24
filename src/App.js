4a9093464e9b2b57664ed498a6785fd9775932b0
Martin <martinlicciardello7@gmail.com>
2022-01-23 22:24:50 -0300

    Barra de navegacion ecomerce

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <header className="App-header">
+        <h1>Proximamente nuestros productos en stock..</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
